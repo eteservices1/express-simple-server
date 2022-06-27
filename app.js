361cc41fe4b945a619e3d4dd590ebf00db1a2bdc
etiennedaherinsta <>
2022-06-27 16:45:37 +0300

    initial

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+// Routes
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+// Listen
+var port = 443;
+app.listen(port);
+console.log('Listening on localhost:'+ port);
